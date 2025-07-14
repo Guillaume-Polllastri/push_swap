a19df6bab8308b895f74cec43202144e4f5fe76d
Guillaume Pollastri <gpollast@f6r6s15.clusters.42paris.fr>
2025-07-14 13:44:40 +0200

    feat sort for 3 numerics

--- src/algo_condition.c
+++ src/algo_condition.c
@@ -6,7 +6,7 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 17:55:04 by gpollast          #+#    #+#             */
-/*   Updated: 2025/07/11 10:27:49 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/07/14 13:43:56 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,21 +81,35 @@ static void	min_to_stack_b(t_stack **a, t_stack **b, int compteur)
 	}
 }
 
+static int	sort_for_3(t_stack **a)
+{
+	t_stack	*tmp;
+
+	tmp = *a;
+	if ((tmp->content < tmp->next->content)
+		&& (tmp->content < tmp->next->next->content))
+		return (sa(*a), rra(a), rra(a), 1);
+	if ((tmp->content > tmp->next->content)
+		&& (tmp->content < tmp->next->next->content))
+		return (sa(*a), 1);
+	if ((tmp->content < tmp->next->content)
+		&& (tmp->content > tmp->next->next->content))
+		return (rra(a), 1);
+	if ((tmp->content > tmp->next->content)
+		&& (tmp->next->content < tmp->next->next->content))
+		return (ra(a), 1);
+	else
+		return (sa(*a), rra(a), 1);
+}
+
 int	cond_sort(t_stack **a, t_stack **b, int nb_elmt)
 {
 	int	compteur;
 
-	if (nb_elmt == 1)
-	{
-		return (1);
-	}
 	if (nb_elmt == 2)
-	{
-		if (is_sort_stack(*a))
-			return (1);
-		ra(a);
-		return (1);
-	}
+		return (ra(a), 1);
+	if (nb_elmt == 3)
+		return (sort_for_3(a));
 	if (nb_elmt <= 5)
 	{
 		compteur = 3;
